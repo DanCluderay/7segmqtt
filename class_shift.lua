d9239f1fda5636a80458b9e262997eab2966c964
dancluderay <dan@approvedfood.co.uk>
2016-10-15 09:09:10 +0100

    Left and Right Single step feature

--- class_shift.lua
+++ class_shift.lua
@@ -20,7 +20,7 @@ gpio.mode(ST, gpio.OUTPUT)
 sda = 2-- SDA Pin
 scl = 1-- SCL Pin
 
-
+StepMode = 1 --Stepmode=1 (Single) , StepMode = 2 (Double)
         
 outputarray = {}
 m1_temp = {}
@@ -168,42 +168,64 @@ end
 function GetNextJobRight(CurrentJob)
 --do a lookup on the current jobstring
 nj = ""
-        if(CurrentJob == "0000")then
+if(StepMode==1) then
+    if(CurrentJob == "0000")then
+        -- This Job 1000
+              nj = "0001";
+        elseif(CurrentJob == "0001")then
+           -- This Job 0100
+              nj = "0010";
+        elseif(CurrentJob == "0010")then
+           -- This Job 0010
+              nj = "0100";          
+        elseif(CurrentJob == "0100")then
+         -- This Job 0001
+              nj = "1000";    
+        elseif(CurrentJob == "1000")then
+         -- This Job 1000
+              nj = "0001";
+        elseif(CurrentJob == "1111")then
+           -- This Job 0100
+              nj = "0000";
+ 
+        end
+end 
+        
+        nextjob=nj
+                -- hold your hourses
+
+end  
+function GetNextJobLeft(CurrentJob)
+--do a lookup on the current jobstring
+nj = ""
+if(StepMode==1) then
+    if(CurrentJob == "0000")then
         -- This Job 1000
               nj = "1000";
-    
-             
-     
         elseif(CurrentJob == "1000")then
            -- This Job 0100
               nj = "0100";
-             
-              
         elseif(CurrentJob == "0100")then
            -- This Job 0010
-              nj = "0010";
-           
-              
+              nj = "0010";          
         elseif(CurrentJob == "0010")then
          -- This Job 0001
-              nj = "0001";
-           
-            
+              nj = "0001";    
         elseif(CurrentJob == "0001")then
          -- This Job 1000
               nj = "1000";
         elseif(CurrentJob == "1111")then
            -- This Job 0100
               nj = "0000";
-             
-              
-        
-           
+ 
         end
+end 
+        
         nextjob=nj
                 -- hold your hourses
 
 end  
+
 
 function module.start()  
 
